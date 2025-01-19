7e0db082466b721e7068006206ff3615ff7cbdc1
marinlauber <marinlauber@tudelft.nl>
2025-01-19 11:43:33 +0100

    fix heart case on GPU

--- example/ThreeD_Heart.jl
+++ example/ThreeD_Heart.jl
@@ -3,26 +3,26 @@ using StaticArrays
 using WriteVTK
 include("../src/utils.jl")
 
-function heart(L=2^5;Re=5e2,mem=Array,U=1,AR=2)
-    # Define simulation size, geometry dimensions, & viscosity
+function heart(L=2^5;Re=5e2,mem=Array,U=1,AR=2,T=Float32)
+    ## Define simulation size, geometry dimensions, & viscosity
     
     # Motion functions
-    ω = 2U/L
-    @fastmath w(t) = 0.1*L*cos(ω*t)
-    @fastmath h(t) = 0.1*L*cos(ω*t)
+    ω = T(2U/L)
+    @fastmath w(t) = 0.1f0*L*cos(ω*t)
+    @fastmath h(t) = 0.1f0*L*cos(ω*t)
     @fastmath r(x) = √(x[1]^2+x[2]^2)
     # Build the heart from a mapped ellipsoid and plane
-    ellipsoid = AutoBody((x,t)->abs(ellipse(x, SA[L,L/AR]))-1.5,
-                         (x,t)->x.-SA[-h(t),-h(t),1.5L+w(t)])
-    disk = AutoBody((x,t)->√(x[3]^2+(r(x)-min(r(x),L/AR))^2)-1.5,
-                    (x,t)->x.-SA[-w(t),-w(t),1.85L])
-    plane = AutoBody((x,t)->x[3]-1.85L)
+    ellipsoid = AutoBody((x,t)->abs(ellipse(x, SA[L,L/AR]))-1.5f0,
+                         (x,t)->x.-SA[-h(t),-h(t),1.5f0*L+w(t)])
+    disk = AutoBody((x,t)->√(x[3]^2+(r(x)-min(r(x),L/AR))^2)-1.5f0,
+                    (x,t)->x.-SA[-w(t),-w(t),1.85f0*L])
+    plane = AutoBody((x,t)->x[3]-1.85f0*L)
     mitral = AutoBody((x,t)->√(x[3]^2+(r(x)-min(r(x),1))^2)-5,
-                      (x,t)->x.-SA[0,0,1.85L])
+                      (x,t)->x.-SA[0,0,1.85f0*L])
     body =  ellipsoid ∩ plane + disk - mitral
 
     # Return initialized simulation
-    Simulation((Int(0.75*L),Int(0.75*L),3L),(0,0,0),L;U,ν=U*L/Re,body,mem,T=Float32)
+    Simulation((Int(0.75*L),Int(0.75*L),3*L),(0f0,0f0,0f0),L;U,ν=U*L/Re,body,mem,T=T)
 end
 
 # using CUDA
